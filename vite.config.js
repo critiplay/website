d9ee96de2851adb5f34a69be72be2a3d38e7321b
Alex Valentim Gonçalves <alex.valentim.goncalves@gmail.com>
2026-04-18 12:12:28 -0300

    Ajustes
    Ajustes

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import compression from 'vite-plugin-compression'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vite.dev/config/
 export default defineConfig({
+    build: {
+        outDir: 'docs',
+        assetsInlineLimit: 100000000,
+        cssCodeSplit: false,
+    },
     plugins: [
         react(),
+        viteSingleFile(),
         compression({
             verbose: true,
             disable: false,
